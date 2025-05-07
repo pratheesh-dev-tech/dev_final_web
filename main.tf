fa2643d168f1cb943aa5528815e320580fa45fce
pratheesh-dev-tech <iq0pooki@gmail.com>
2025-05-08 00:51:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"  
 }
 
-resource "aws_s3_bucket" "static_website" {
-  bucket = "goblinpk123"
-  force_destroy = true
-
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
   tags = {
-    Name        = "StaticWebsiteBucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "static_website" {
-  bucket = aws_s3_bucket.static_website.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.static_website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.static_website.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.static_website.arn}/*"
-      }
-    ]
-  })
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.static_website.id
-
-  versioning_configuration {
-    status = "Enabled"
+      Name = "TF-Instance"
   }
 }
